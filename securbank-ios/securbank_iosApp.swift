416276a94a64e420da19d852a8dd2439e8120696
Mark Szulc <mszulc@adobe.com>
2023-05-21 14:14:19 +1000

    Initial Commit

---
+++ securbank-ios/securbank_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  securbank_iosApp.swift
+//  securbank-ios
+//
+//  Created by Mark Szulc on 21/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct securbank_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
